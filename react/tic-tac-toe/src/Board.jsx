90fa06c351d80883387e8689d613a47651e6d88a
mugupta <mukesh.gupta@priceline.com>
2019-10-18 03:50:07 -0400

    add create square board and game components

---
+++ react/tic-tac-toe/src/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Square from './Square';
+
+class Board extends React.Component{
+    render(){
+        return <Square/>
+    }
+}
+
+export default Board
---
+++ react/tic-tac-toe/src/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+class Square extends React.Component{
+
+    render(){
+        return (
+            <h1>Tic tac toe</h1>
+        )
+    }
+}
+
+export default Square
