f7fa0b38760c51d6b24c5f71560c486f5ba99f0a
mugupta <mukesh.gupta@priceline.com>
2019-10-20 05:17:01 -0400

    move state from squares to board

--- react/tic-tac-toe/src/square/Square.jsx
+++ react/tic-tac-toe/src/square/Square.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 class Square extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            value : null,
-        }
     }
     render(){
         return (
            <button className="square" 
-           onClick={ ()=>this.setState({value:'X'})}>
-           {this.state.value}
+           onClick={ ()=>this.props.onClick()}>
+           {this.props.value}
            </button>
         )
     }
